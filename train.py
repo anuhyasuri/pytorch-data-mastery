0b42e77ffeffbe644049a3eee9ee875635f5a65f
anuhyasuri <anuhyasuri@gmail.com>
2026-03-05 22:05:17 -0800

    Updated libraries

--- train.py
+++ train.py
@@ -15,7 +15,7 @@ def main():
     ])
 
     train_dataset = MyImageDataset(root_dir = "data/archive/seg_train/seg_train", transform = my_transforms)
-    train_dataloader = DataLoader(train_dataset, batch_size = 32, shuffle = True, num_workers = 8)
+    train_dataloader = DataLoader(train_dataset, batch_size = 32, shuffle = True, num_workers = 0)
 
     images, labels = next(iter(train_dataloader))
     print(f"Batch shape: {images.shape}")
