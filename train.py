4c7362490c7123c32f6d953d17457ee01c7416eb
anuhyasuri <anuhyasuri@gmail.com>
2026-04-21 22:42:06 -0700

    Added code to load the huggingface dataloader

--- train.py
+++ train.py
@@ -3,8 +3,10 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 from src.datasets import MyImageDataset
 from src.utils import visualize_data
+from src.hf_pipeline import get_hf_loader
 def main():
 
+    # Pytorch dataset
     my_transforms = transforms.Compose([
     transforms.Resize((224,224)), # Standard resizing
     transforms.ToTensor(), # Convert to a tensor
@@ -22,6 +24,11 @@ def main():
     print("Visualizing data")
     visualize_data(train_dataloader, train_dataset.class_names)
     print("Done!")
+
+    # Hugging face dataset
+    hf_loader = get_hf_loader(dataset = "ethz/food101", batch_size = 32)
+    data_batch = next(iter(hf_loader))
+    print("HF",data_batch)
 
 if __name__ == '__main__':
     main()
