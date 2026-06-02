5fdb2d72d548bdd9dee559397d80bce498b7d470
Anuhya Suri <anuhyasuri@gmail.com>
2026-06-02 23:18:30 +0000

    Added validation loop

--- src/engine.py
+++ src/engine.py
@@ -2,9 +2,11 @@ import torch
 from tqdm.auto import tqdm
 
 def train_one_epoch(model, dataloader, loss_fn, optimizer, device):
+  """Trains Model"""
   model.train()
   running_loss = 0.0
   total_samples = 0
+  correct_predictions = 0
 
   progress_bar = tqdm(dataloader, desc="Training batches", leave=False)
 
@@ -28,4 +30,37 @@ def train_one_epoch(model, dataloader, loss_fn, optimizer, device):
   epoch_accuracy = (correct_predictions / total_samples) * 100
 
   return epoch_loss, epoch_accuracy
+
+def validate_one_epoch(model, dataloader, loss_fn, device):
+  """Evaluates Model Performance"""
+  model.eval()
+  running_loss = 0.0
+  total_samples = 0
+  correct_predictions = 0
+
+  with torch.no_grad():
+    progress_bar = tqdm(dataloader, desc="Validation", leave=False)
+    for images, labels in progress_bar:
+      images, labels = images.to(device), labels.to(device)
+      # Forward pass
+      logits = model(images)
+      # Calculate loss
+      loss = loss_fn(logits)
+
+      running_loss +=loss.item()*images.size(0)
+      _,predictions = torch.max(logits, dim=1)
+
+      correct_predictions += (predictions == labels).sum().item()
+      total_samples += labels.size(0)
+            
+  epoch_loss = running_loss / total_samples
+  epoch_accuracy = (correct_predictions / total_samples) * 100
+    
+  return epoch_loss, epoch_accuracy
+
+
+
+
+
+
 
